29e86784f88591aff5f9dbd28dc9287d5d8ff18b
laughing-coder <laughinl_coder@protonmail.com>
2021-08-19 00:31:34 +0200

    add CBC module, add NYT module(description and links)

---
+++ news_parser.py
@@ -0,0 +1,55 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+
+
+#CBC
+url_cbc = "https://www.cbc.ca/news/world"
+request = requests.get(url_cbc)
+
+soup_cbc = BeautifulSoup(request.text, "html.parser")
+
+header_cbc = soup_cbc.find_all("div", class_="card")
+
+for description_cbc in header_cbc:
+    description_cbc = description_cbc.find("a", {'class': 'contentWrapper'})
+
+    if description_cbc is not None:
+        link_cbc = description_cbc.get('href')
+
+    print(str(description_cbc.text) + " \n" + str("https://www.cbc.ca"+link_cbc))
+    print("******************************")
+
+
+#NYT
+url_nyt = "https://www.nytimes.com/section/world"
+request = requests.get(url_nyt)
+
+soup_nyt = BeautifulSoup(request.text, "html.parser")
+
+header_nyt = soup_nyt.find_all("li", class_="css-ye6x8s")
+
+for description_nyt in header_nyt:
+    description_nyt = description_nyt.find("div", {'class': 'css-1l4spti'}).find('a')
+
+    if description_nyt is not None:
+        link_nyt = description_nyt.get('href')
+        print(str(description_nyt.text) + " \n" + str("https://www.nytimes.com/"+link_nyt))
+        print("******************************")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
